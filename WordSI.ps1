08f9c66b8e006c34018053486183533fcd0d97d5
FireLies <jeantitor@protonmail.com>
2022-03-23 00:10:46 +0700

    Fixed efficiency issues

--- WordSI.ps1
+++ WordSI.ps1
@@ -1,14 +1,16 @@
 Clear-Host
 
-[System.Collections.ArrayList][string[]]$TrainData = Get-Content .\DataSet\Train_DataSet.txt
-$TrainData = $TrainData.ToArray()
+if ($TrainData.Count -eq 0) {
+    [System.Collections.ArrayList][string[]]$TrainData = Get-Content .\DataSet\Train_DataSet.txt
+    $TrainData = $TrainData.ToArray()
 
-Write-Host "$($TrainData.Count) Words has been loaded."
+    '{0} Words has been loaded.' -f $TrainData.Count 
+}
 
 function Get-Input {
 
-    $NumOf_Input = 15
-    $Inputs = 1..$NumOf_Input | ForEach-Object {
+    $NumOf_Input = 10
+    $Inputs = foreach ($i in 1..$NumOf_Input) {
         $Randomize = ($TrainData | Get-Random).replace(' ', '')
         [PSCustomObject]@{
             Word = $Randomize
@@ -22,36 +24,37 @@ function Get-Input {
 function Guess ($Inputs, $Weight) {
 
     [System.Collections.ArrayList][float[]]$Sum = @()
+    [System.Collections.ArrayList][int[]]$Guess = @()
     [System.Collections.ArrayList][int[]]$Err = @()
 
-    for ($i = 0; $i -lt $Inputs.Count; $i++) {
-        [void]$Sum.Add(($Inputs.Characters[$i] * $Weight[$i]))
-        $Sum[$i] = [math]::Round($Sum[$i], 2)
-    }
+    foreach ($i in 1..$Inputs.Count) {
+        [void]$Sum.Add($Inputs.Characters[$i-1] * $Weight[$i-1])
+        [void]$(switch ($Sum[$i-1]) {
+            {$_ -ne $Inputs.Characters[$i-1]} {$Guess.Add(0)}
+            default {$Guess.Add(1)}
+        })
 
-    for ($i = 0; $i -lt $Inputs.Count; $i++) {
-        [int[]]$Guess += switch ($Sum[$i]) {
-            {$_ -ne $Inputs.Characters[$i]} {0}
-            {$_ -eq $Inputs.Characters[$i]} {1}
-        }
-
-        [void]$Err.Add(1 - $Guess[$i])
+        [void]$Err.Add(1 - $Guess[$i-1])
     }
 
     $Result = 1..$Inputs.Count | ForEach-Object {
         [PSCustomObject]@{
-            Sum = $Sum[$_]
-            Guess = $Guess[$_]
-            Words = $Inputs.Word[$_]
+            Sum = '{0:n2}' -f $Sum[$_-1]
+            Guess = '  {0}' -f $Guess[$_-1]
+            Words = $Inputs.Word[$_-1]
         }
     }
 
     $Result
+
+    ""; 'Correct guess: {0}' -f ($Guess | Select-String '1').Count
 }
 
 $Inputs = Get-Input
 [float[]]$Weight = (
-    1..$Inputs.Count | ForEach-Object {[math]::Round((Get-Random -Max 1.0 -Min 0.1), 2)}
+    1..$Inputs.Count | ForEach-Object {
+        '{0:n2}' -f (Get-Random -Max 1.0 -Min 0.1)
+    }
 )
 
 Guess $Inputs $Weight
